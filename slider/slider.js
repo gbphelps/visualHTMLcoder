bd7ee8c1421209218dde3309d932c71337c91a73
Grant Phelps <gbphelps2@gmail.com>
2018-08-11 11:38:28 -0400

    Add preliminary color swatch

--- slider/slider.js
+++ slider/slider.js
@@ -103,60 +103,88 @@ const updateDragger2 = () => {
   const dragger2 = document.getElementById('dragger2');
   const canvas = document.getElementById('canvas');
 
-  const xp = dragger2.x;
-  const yp = canvas.height - dragger2.y;
+    const xp = dragger2.x;
+    const yp = canvas.height - dragger2.y;
 
-  let theta0 = Math.atan(yp/xp);
-  const theta = (Math.PI/3 - theta0)/(Math.PI/3);
+    let theta0 = Math.atan(yp/xp);
+    const theta = (Math.PI/3 - theta0)/(Math.PI/3);
 
-  let r = Math.sqrt(xp*xp + yp*yp)/300;
-  r = r * Math.cos(Math.PI/6 - theta0) / (Math.sqrt(3)/2)
+    let r = Math.sqrt(xp*xp + yp*yp)/300;
+    r = r * Math.cos(Math.PI/6 - theta0) / (Math.sqrt(3)/2);
 
 
-    let R = (canvas.color[0] + (255-canvas.color[0])*theta) * r;
-    let G = (canvas.color[1] + (255-canvas.color[1])*theta) * r;
-    let B = (canvas.color[2] + (255-canvas.color[2])*theta) * r;
+    let R = Math.round((canvas.color[0] + (255-canvas.color[0])*theta) * r) || 0;
+    let G = Math.round((canvas.color[1] + (255-canvas.color[1])*theta) * r) || 0;
+    let B = Math.round((canvas.color[2] + (255-canvas.color[2])*theta) * r) || 0;
     let A = 255;
 
-    console.log(R,G,B,A);
-
+    dragger2.color = [R,G,B,A]
     dragger2.style.background = format([R,G,B,A]);
 }
 
+
+const updateSwatch = () => {
+  const swatch = document.getElementById('swatch');
+  swatch.style.background = format(dragger2.color);
+  swatch.innerHTML = format(dragger2.color);
+};
+
+const create = (tag, parent, props, style) => {
+  const element = document.createElement(tag);
+  Object.assign(element, props);
+  parent.append(element);
+  if (style) Object.assign(element.style, style);
+  return element;
+};
 
 
 document.addEventListener('DOMContentLoaded',()=>{
 
   const picker = document.getElementById('picker');
 
-  const canvas = document.createElement('CANVAS');
-  canvas.id = 'canvas';
-  canvas.height = Math.round(300*Math.sqrt(3)/2);
-  canvas.width = 300;
-  canvas.color = [255, 0, 0, 255];
-  picker.append(canvas);
+  const canvas = create('CANVAS', picker, {
+    id: 'canvas',
+    height: Math.round(300*Math.sqrt(3)/2),
+    width: 300,
+    color: [255, 0, 0, 255],
+  });
 
   const ctx = canvas.getContext('2d');
 
-  const slider = document.createElement('DIV');
-  slider.id = 'slider';
-  slider.style.position = 'absolute';
-  picker.append(slider);
+  const slider =
+    create('DIV', picker, {
+      id: 'slider'
+    },{
+      position: 'relative'
+    });
 
-  const dragger = document.createElement('DIV');
-  dragger.id = 'dragger';
-  dragger.style.left = -9 + 'px'; //TODO 1/2 dragger height
-  dragger.style.position = 'absolute';
-  dragger.status = 0;
-  slider.append(dragger);
+  const dragger =
+    create('DIV', slider, {
+      id: 'dragger',
+      status: 0,
+    },{
+      left: -9 + 'px',
+      position: 'absolute',
+    });
 
-  const dragger2 = document.createElement('DIV');
-  dragger2.id = 'dragger2';
-  dragger2.style.left = canvas.width / 2 - 9 + 'px'; //TODO 1/2 dragger height;
-  dragger2.style.top = -9 + 'px';
-  dragger2.x = canvas.width / 2;
-  dragger2.y = 0;
-  picker.append(dragger2);
+  const dragger2 =
+    create('DIV', picker, {
+      id: 'dragger2',
+      x: canvas.width/2,
+      y: 0,
+    },{
+      left: canvas.width/2 - 9 + 'px',
+      top: -9 + 'px'
+    });
+
+  const swatch =
+    create('DIV', picker,{
+      id: 'swatch',
+      innerHTML: format(canvas.color),
+    },{
+      background: format(canvas.color),
+    });
+
 
   updateDragger2();
   updateCanvas();
@@ -200,20 +228,10 @@ document.addEventListener('DOMContentLoaded',()=>{
       y = e.clientY;
 
 
-      // //TODO just recalculate color - there are too many broken points
-      // const color = ctx.getImageData(dragger2.x, dragger2.y, 1, 1).data;
-      // dragger2.style.background = format(color);
-      //
-      // if (dragger2.y === canvas.height){
-      //   const sat = (dragger2.x / canvas.width)*255;
-      //   dragger2.style.background = `rgba(${sat},${sat},${sat},255)`;
-      // }
-
-
-
       dragger2.style.left = dragger2.x - 9 + 'px';
       dragger2.style.top = dragger2.y - 9 + 'px';
       updateDragger2();
+      updateSwatch();
 
 
 
@@ -231,6 +249,7 @@ document.addEventListener('DOMContentLoaded',()=>{
   });
 
   dragger.addEventListener('mousedown', e => {
+    e.preventDefault();
     let x = e.clientX;
 
     const mousemove = e => {
@@ -253,6 +272,7 @@ document.addEventListener('DOMContentLoaded',()=>{
 
       updateCanvas();
       updateDragger2();
+      updateSwatch();
 
     };
 
