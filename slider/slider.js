6bab2cc247b204af307e24c743cdec40d8de829f
Grant Phelps <gbphelps2@gmail.com>
2018-08-11 10:28:51 -0400

    Refactor by attaching variables as attributes, two-way binding for draggers

--- slider/slider.js
+++ slider/slider.js
@@ -26,20 +26,21 @@ const format = color => `rgba(${color[0]},${color[1]},${color[2]},${color[3]})`;
 
 
 
-const updateCanvas = (color, ctx) => {
-  let height = document.getElementById('canvas').height;
-  let width = document.getElementById('canvas').width;
-  document.getElementById('dragger').style.background = format(color);
+const updateCanvas = () => {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
 
-  const buffer = ctx.createImageData(width, height);
+  document.getElementById('dragger').style.background = format(canvas.color);
+
+  const buffer = ctx.createImageData(canvas.width, canvas.height);
 
 
-  for (let i=0; i<(height*width); i++) {
-    const top = Math.floor(i / width);
-    const left = Math.floor(i % width);
+  for (let i=0; i<(canvas.height* canvas.width); i++) {
+    const top = Math.floor(i / canvas.width);
+    const left = Math.floor(i % canvas.width);
 
     const xp = left;
-    const yp = height - top;
+    const yp = canvas.height - top;
 
 
     // RADIAL COORDINATES
@@ -53,9 +54,9 @@ const updateCanvas = (color, ctx) => {
 
     if (theta < 0 || theta > 1 || r > 1){
     }else{
-      buffer.data[i*4 + 0] = (color[0] + (255-color[0])*theta) * r;
-      buffer.data[i*4 + 1] = (color[1] + (255-color[1])*theta) * r;
-      buffer.data[i*4 + 2] = (color[2] + (255-color[2])*theta) * r;
+      buffer.data[i*4 + 0] = (canvas.color[0] + (255-canvas.color[0])*theta) * r;
+      buffer.data[i*4 + 1] = (canvas.color[1] + (255-canvas.color[1])*theta) * r;
+      buffer.data[i*4 + 2] = (canvas.color[2] + (255-canvas.color[2])*theta) * r;
       buffer.data[i*4 + 3] = 255;
     }
 
@@ -71,30 +72,56 @@ const updateCanvas = (color, ctx) => {
   ctx.putImageData(buffer, 0, 0);
 
   //for anti-aliasing
-  const g1 = ctx.createLinearGradient(0, 0, width/2, 0);
+  const g1 = ctx.createLinearGradient(0, 0, canvas.width/2, 0);
   g1.addColorStop(0, 'black');
-  g1.addColorStop(1, format(color));
+  g1.addColorStop(1, format(canvas.color));
 
-  const g2 = ctx.createLinearGradient(width/2, 0, width, 0);
-  g2.addColorStop(0, format(color));
+  const g2 = ctx.createLinearGradient(canvas.width/2, 0, canvas.width, 0);
+  g2.addColorStop(0, format(canvas.color));
   g2.addColorStop(1, 'white');
 
   ctx.beginPath();
-    ctx.moveTo(1, height -.5);
-    ctx.lineTo(width/2 + .5, .5);
+    ctx.moveTo(1, canvas.height -.5);
+    ctx.lineTo(canvas.width/2 + .5, .5);
     ctx.strokeStyle = g1;
     ctx.stroke();
   ctx.closePath();
 
   ctx.beginPath();
-    ctx.moveTo(width/2 + .5, .5);
-    ctx.lineTo(width, height -.5);
+    ctx.moveTo(canvas.width/2 + .5, .5);
+    ctx.lineTo(canvas.width, canvas.height -.5);
     ctx.strokeStyle = g2;
     ctx.stroke();
   ctx.closePath();
 
 };
 
+
+
+
+const updateDragger2 = () => {
+  const dragger2 = document.getElementById('dragger2');
+  const canvas = document.getElementById('canvas');
+
+  const xp = dragger2.x;
+  const yp = canvas.height - dragger2.y;
+
+  let theta0 = Math.atan(yp/xp);
+  const theta = (Math.PI/3 - theta0)/(Math.PI/3);
+
+  let r = Math.sqrt(xp*xp + yp*yp)/300;
+  r = r * Math.cos(Math.PI/6 - theta0) / (Math.sqrt(3)/2)
+
+
+    let R = (canvas.color[0] + (255-canvas.color[0])*theta) * r;
+    let G = (canvas.color[1] + (255-canvas.color[1])*theta) * r;
+    let B = (canvas.color[2] + (255-canvas.color[2])*theta) * r;
+    let A = 255;
+
+    console.log(R,G,B,A);
+
+    dragger2.style.background = format([R,G,B,A]);
+}
 
 
 
@@ -106,6 +133,7 @@ document.addEventListener('DOMContentLoaded',()=>{
   canvas.id = 'canvas';
   canvas.height = Math.round(300*Math.sqrt(3)/2);
   canvas.width = 300;
+  canvas.color = [255, 0, 0, 255];
   picker.append(canvas);
 
   const ctx = canvas.getContext('2d');
@@ -130,8 +158,8 @@ document.addEventListener('DOMContentLoaded',()=>{
   dragger2.y = 0;
   picker.append(dragger2);
 
-
-  updateCanvas(calcColor(0),ctx);
+  updateDragger2();
+  updateCanvas();
 
 
   dragger2.addEventListener('mousedown', e => {
@@ -146,18 +174,6 @@ document.addEventListener('DOMContentLoaded',()=>{
       const newy = dragger2.y + diffy;
 
       const box = canvas.getBoundingClientRect();
-
-      if (e.clientX > box.right){
-        dragger2.x = canvas.width;
-      }else if (e.clientX < box.left){
-        dragger2.x = 0;
-      }
-
-      if (e.clientY > box.bottom){
-        dragger2.y = canvas.height;
-      }else if (e.clientY < box.right){
-        dragger2.y = 0;
-      }
 
       if (-canvas.height*(2*newx/canvas.width - 1) > newy){
         dragger2.x = newx;
@@ -184,18 +200,22 @@ document.addEventListener('DOMContentLoaded',()=>{
       y = e.clientY;
 
 
-      //TODO just recalculate color - there are too many broken points
-      const color = ctx.getImageData(dragger2.x, dragger2.y, 1, 1).data;
-      dragger2.style.background = format(color);
-      if (dragger2.y === canvas.height){
-        const sat = (dragger2.x / canvas.width)*255;
-        dragger2.style.background = `rgba(${sat},${sat},${sat},255)`;
-      }
+      // //TODO just recalculate color - there are too many broken points
+      // const color = ctx.getImageData(dragger2.x, dragger2.y, 1, 1).data;
+      // dragger2.style.background = format(color);
+      //
+      // if (dragger2.y === canvas.height){
+      //   const sat = (dragger2.x / canvas.width)*255;
+      //   dragger2.style.background = `rgba(${sat},${sat},${sat},255)`;
+      // }
 
 
 
       dragger2.style.left = dragger2.x - 9 + 'px';
       dragger2.style.top = dragger2.y - 9 + 'px';
+      updateDragger2();
+
+
 
 
 
@@ -229,7 +249,10 @@ document.addEventListener('DOMContentLoaded',()=>{
       dragger.style.left = dragger.status - 9 + 'px'; //TODO 1/2 dragger height
 
       const colorArr = calcColor(dragger.status);
-      updateCanvas(colorArr, ctx);
+      canvas.color = colorArr;
+
+      updateCanvas();
+      updateDragger2();
 
     };
 
