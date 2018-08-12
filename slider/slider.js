7d2f17d99cfb0e91089927e78ae3e4a02c89b773
Grant Phelps <gbphelps2@gmail.com>
2018-08-12 13:15:05 -0400

    Comment out changes to slider gradient saturation

--- slider/slider.js
+++ slider/slider.js
@@ -149,45 +149,45 @@ const create = (tag, parent, props, style) => {
 const updateSlider = () => {
 
 const dragger2 = document.getElementById('dragger2');
-const canvas = document.getElementById('canvas');
-
-  const xp = dragger2.x;
-  const yp = canvas.height - dragger2.y;
-
-  let theta0 = Math.atan(yp/xp);
-  const theta = (Math.PI/3 - theta0)/(Math.PI/3);
-
-  let r = Math.sqrt(xp*xp + yp*yp)/300;
-  r = r * Math.cos(Math.PI/6 - theta0) / (Math.sqrt(3)/2);
-
-  let colors = [
-    [255, 0, 0],
-    [255, 255, 0],
-    [0, 255, 0],
-    [0, 255, 255],
-    [0, 0, 255],
-    [255, 0, 255],
-    [255, 0, 0]
-  ];
-
-  const colorStops = colors.map(color => {
-    const three = color.map(component => {
-      return Math.round((component + (255 - component)*theta) * r) || 0
-    });
-    return three.concat(255);
-  });
-
-  document.getElementById('slider').style.background =
-  `linear-gradient(
-        to bottom,
-        ${format(colorStops[0])},
-        ${format(colorStops[1])},
-        ${format(colorStops[2])},
-        ${format(colorStops[3])},
-        ${format(colorStops[4])},
-        ${format(colorStops[5])},
-        ${format(colorStops[6])}
-    )`;
+// const canvas = document.getElementById('canvas');
+//
+//   const xp = dragger2.x;
+//   const yp = canvas.height - dragger2.y;
+//
+//   let theta0 = Math.atan(yp/xp);
+//   const theta = (Math.PI/3 - theta0)/(Math.PI/3);
+//
+//   let r = Math.sqrt(xp*xp + yp*yp)/300;
+//   r = r * Math.cos(Math.PI/6 - theta0) / (Math.sqrt(3)/2);
+//
+//   let colors = [
+//     [255, 0, 0],
+//     [255, 255, 0],
+//     [0, 255, 0],
+//     [0, 255, 255],
+//     [0, 0, 255],
+//     [255, 0, 255],
+//     [255, 0, 0]
+//   ];
+//
+//   const colorStops = colors.map(color => {
+//     const three = color.map(component => {
+//       return Math.round((component + (255 - component)*theta) * r) || 0
+//     });
+//     return three.concat(255);
+//   });
+//
+//   document.getElementById('slider').style.background =
+//   `linear-gradient(
+//         to bottom,
+//         ${format(colorStops[0])},
+//         ${format(colorStops[1])},
+//         ${format(colorStops[2])},
+//         ${format(colorStops[3])},
+//         ${format(colorStops[4])},
+//         ${format(colorStops[5])},
+//         ${format(colorStops[6])}
+//     )`;
 
   updateDragger();
 
@@ -239,19 +239,6 @@ document.addEventListener('DOMContentLoaded',()=>{
         position: 'absolute',
         'border-right-color': format(canvas.color)
       });
-
-    // create('DIV', dragger, {}, {
-    //   position:'absolute',
-    //   'border': '1px solid transparent',
-    //   'border-width': '17px',
-    //   'border-right-color': 'gray',
-    //   height: '0px',
-    //   width: '0px',
-    //   top: '-17px',
-    //   left: '-18px',
-    //   'z-index': -1
-    // })
-
 
 
   const swatch =
