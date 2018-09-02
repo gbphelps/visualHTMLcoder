b74d10104354ab59b97eee1dab9fe142d11a7ac0
Grant Phelps <gbphelps2@gmail.com>
2018-09-02 16:21:16 -0400

    Apply smooting to both sides of slider, identify boundary conditions with black pixels

--- javascripts/slider/initializers/initializeCircleSlider.js
+++ javascripts/slider/initializers/initializeCircleSlider.js
@@ -7,6 +7,7 @@ export const initializeCircleSlider = (canvasWidth, spectrumWidth, dragDiam) => 
     id: 'spectrumContainer',
     width: canvasWidth
   },{
+    //background: 'black',
     height: canvasWidth + 'px',
     width: canvasWidth + 'px',
     position: 'relative'
@@ -48,7 +49,6 @@ const fillPixel = (buffer, i, color) => {
   buffer.data[i*4 + 0] = color[0];
   buffer.data[i*4 + 1] = color[1];
   buffer.data[i*4 + 2] = color[2];
-  buffer.data[i*4 + 3] = 255;
 };
 
 
@@ -61,6 +61,7 @@ const populateBuffer = (buffer, innerRadius, curve) => {
     let bottom = circleSlider.width - Math.floor(i / circleSlider.width);
     const y = bottom - circleSlider.width/2;
 
+    if (x === 0 && y === 0) continue;
 
     let progress=0;
     if (x < 0){
@@ -71,40 +72,18 @@ const populateBuffer = (buffer, innerRadius, curve) => {
 
     const color = calcColor(progress);
 
+    fillPixel(buffer,i,color);
     const rSquare = x*x + y*y;
-    if ( rSquare < circleSlider.width*circleSlider.width/4 && rSquare > innerRadius* innerRadius){
-      fillPixel(buffer,i,color)
+    if ( rSquare <= circleSlider.width*circleSlider.width/4 && rSquare >= innerRadius* innerRadius){
+      buffer.data[i*4 + 3] = 255;
     }
 
 
-    const xabs = Math.abs(x);
-    const yabs = Math.abs(y);
 
-    if (
-      //top collision
-      xabs < curve.outer[yabs] &&
-      curve.outer[yabs] < xabs+1
-    ){
-      fillPixel(buffer,i,[0,0,0])
-    }else if (
-      //bottom collision
-      xabs < curve.outer[yabs+1] &&
-      curve.outer[yabs+1] < xabs+1
-    ){
-      fillPixel(buffer,i,[0,0,0])
-    }else if (
-      //left collision
-      yabs < curve.outer[xabs] &&
-      curve.outer[xabs] < yabs+1
-    ){
-      fillPixel(buffer,i,[0,0,0])
-    }else if (
-      //right collision
-      yabs < curve.outer[xabs+1] &&
-      curve.outer[xabs+1] < yabs+1
-    ){
-      fillPixel(buffer,i,[0,0,0])
-    }
+
+    calcCollisions(x,y,curve, buffer, i);
+
+
 
   }
 
@@ -152,3 +131,193 @@ const boundaryMap = (oR, iR) => {
   }
   return { outer, inner };
 }
+
+
+
+
+
+
+
+
+const collisionMap = (x, y, curve) => {
+  const xabs = Math.abs(x);
+  const yabs = Math.abs(y);
+  let l,r,t,b;
+
+  if (
+    //top collision
+    xabs <= curve[yabs] &&
+    curve[yabs] < xabs+1
+  ){
+    t = curve[yabs] - xabs;
+  }
+
+  if (
+    //bottom collision
+    xabs <= curve[yabs+1] &&
+    curve[yabs+1] < xabs+1
+  ){
+    b = curve[yabs + 1] - xabs;
+  }
+
+  if (
+    //left collision
+    yabs <= curve[xabs] &&
+    curve[xabs] < yabs+1
+  ){
+    l = curve[xabs] - yabs;
+  }
+
+  if (
+    //right collision
+    yabs <= curve[xabs+1] &&
+    curve[xabs+1] < yabs+1
+  ){
+    r = curve[xabs + 1] - yabs;
+  }
+
+  return { l, r, t, b }
+}
+
+
+
+
+const gt = (x,y) => x >= y;
+const lt = (x,y) => x <= y;
+
+const orient = (x, y, cb, radius) => {
+  let orientation;
+
+  if (cb(x*x + y*y, radius*radius)){
+    orientation = {x:0, y:0}
+  } else if (cb(x*x + (y+1)*(y+1), radius*radius)) {
+    orientation = {x:0, y:1}
+  } else if (cb((x+1)*(x+1) + y*y, radius*radius)) {
+    orientation = {x:1, y:0}
+  } else {
+    orientation = {x:1, y:1}
+  }
+
+  return orientation;
+}
+
+
+const setOpacity = (orientation,buffer,i,l,r,t,b,lt) => {
+  let opacity;
+
+  if (typeof(b) === 'number' && typeof(t) === 'number'){
+    opacity = (
+      orientation.x === 0 ?
+        (b + t)/2 :
+        1 - (b + t)/2
+    )
+    buffer.data[i*4 + 3] = opacity*255
+  } else if (typeof(l) ==='number' && typeof(r) === 'number'){
+    opacity = (
+      orientation.y === 0 ?
+        (l + r)/2 :
+        1 - (l + r)/2
+    )
+    buffer.data[i*4 + 3] = opacity*255
+  } else if (typeof(r) === 'number' && typeof(b) === 'number'){
+      opacity = lt ? 1 : 0;
+      buffer.data[i*4 + 3] = opacity*255
+  } else if ( typeof(l) ==='number' && typeof(t) === 'number'){
+      opacity = lt ? 0 : 1;
+      buffer.data[i*4 + 3] = opacity*255
+  }
+  // if (l||r||t||b) buffer.data[i*4 + 3] = opacity*255;
+  if (!!l + !!r + !!t + !!b === 1){
+    fillPixel(buffer,i,[0,0,0]);
+    buffer.data[i*4 + 3] = 255;
+  }
+}
+
+
+
+const calcCollisions = (x, y, curve, buffer, i) => {
+
+  const oR = circleSlider.width/2;
+  const iR = circleSlider.width/2 - circleSlider.thickness;
+
+  const xabs = Math.abs(x);
+  const yabs = Math.abs(y);
+  let l,r,t,b, orientation;
+
+
+  ({l,r,t,b} = collisionMap(x, y, curve.outer));
+  orientation = orient(x, y, lt, oR);
+
+  setOpacity(orientation,buffer,i,l,r,t,b,true);
+
+  ({l,r,t,b} = collisionMap(x, y, curve.inner));
+  orientation = orient(x, y, gt, iR);
+
+  setOpacity(orientation,buffer,i,l,r,t,b, false);
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//NOTE there's something wrong with triangular cutouts (case 3-6, not 1-2)
+// const setOpacity = (orientation,buffer,i,l,r,t,b,lt) => {
+//   let opacity;
+//
+//   if (typeof(b) === 'number' && typeof(t) === 'number'){
+//     opacity = (
+//       orientation.x === 0 ?
+//         (b + t)/2 :
+//         1 - (b + t)/2
+//     )
+//     buffer.data[i*4 + 3] = opacity*255;
+//     console.log(orientation);
+//     }else if (typeof(l) ==='number' && typeof(r) === 'number'){
+//     opacity = (
+//       orientation.y === 0 ?
+//         (l + r)/2 :
+//         1 - (l + r)/2
+//     )
+//   buffer.data[i*4 + 3] = opacity*255;
+// }else if (typeof(l) === 'number' && typeof(b) === 'number'){
+//     const base = l;
+//     const height = b;
+//     const area = base * height / 2;
+//     opacity = lt ? 0 : 0;
+//     buffer.data[i*4 + 3] = opacity*255;
+//
+//   }else if (typeof(r) === 'number' && typeof(b) === 'number'){
+//     const base = 1-r;
+//     const height = b;
+//     const area = base * height / 2;
+//     opacity = lt ? 1 : 0;
+//     buffer.data[i*4 + 3] = opacity*255;
+//
+//   }else if ( typeof(l) ==='number' && typeof(t) === 'number'){
+//     const base = l;
+//     const height = 1-t;
+//     const area = base * height / 2;
+//     opacity = lt ? 0 : 1;
+//     buffer.data[i*4 + 3] = opacity*255;
+//
+//   }else if (typeof(r) === 'number' && typeof(t) ==='number'){
+//     const base = 1-r;
+//     const height = 1-t;
+//     const area = base * height / 2;
+//     opacity = lt ? 0 : 0;
+//     buffer.data[i*4 + 3] = opacity*255;
+//
+//   }
+// }
