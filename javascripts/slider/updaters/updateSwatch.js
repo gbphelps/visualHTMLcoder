0e264439afa4de0896cdaaf105cdd793703c743f
Grant Phelps <gbphelps2@gmail.com>
2018-09-04 22:45:03 -0400

    Fix grayscale conversion

--- javascripts/slider/updaters/updateSwatch.js
+++ javascripts/slider/updaters/updateSwatch.js
@@ -3,8 +3,13 @@ import { format } from '../utils'
 export const updateSwatch = () => {
   const swatch = document.getElementById('swatch');
   swatch.style.background = format(dragger2.color);
-  const sat = dragger2.color.reduce((acc,el) => acc += el)-255;
-  swatch.style.color = (sat < 255*3/2 ? 'white' : 'black');
+  const [R,G,B] = dragger2.color;
+  const sat =
+    .2126 * R +
+    .7152 * G +
+    .0722 * B ;
+    console.log(sat);
+  swatch.style.color = (sat < 255/2 ? 'white' : 'black');
    dragger2.color.forEach((hue,i)=>{
      const input = document.getElementById(`color-${i}`);
      input.value = hue;
